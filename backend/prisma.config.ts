18ea57395e015d98fcc19e8e87c541c436e17242
xenon <demiladeafuape74@gmail.com>
2026-04-04 00:54:38 +0100

    fixed issue with prisma nd db connection nd also created the tables

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,14 +1,12 @@
-// // prisma.config.ts
+// prisma.config.ts
+import { defineConfig } from 'prisma/config'
+import * as dotenv from 'dotenv'
 
-// import path from 'node:path'
-// import { defineConfig } from 'prisma/config'
-// import { PrismaPg } from '@prisma/adapter-pg'
+dotenv.config()
 
-// export default defineConfig({
-//   schema: path.join('prisma', 'schema.prisma'),
-//   migrate: {
-//     async adapter(env) {
-//       return new PrismaPg({ connectionString: env.DATABASE_URL })
-//     },
-//   },
-// })
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+  datasource: {
+    url: process.env.DIRECT_URL!,
+  },
+})
