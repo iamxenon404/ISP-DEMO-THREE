75c97620283d745a1f959b8b69c8fcf64c751b57
xenon <demiladeafuape74@gmail.com>
2026-04-04 00:22:33 +0100

    updated prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,14 @@
+// prisma.config.ts
+
 import path from 'node:path'
 import { defineConfig } from 'prisma/config'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
+  migrate: {
+    async adapter(env) {
+      return new PrismaPg({ connectionString: env.DATABASE_URL })
+    },
+  },
 })
