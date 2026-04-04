094cff2ab638f12374f2574ae3f602afdab4c108
xenon <demiladeafuape74@gmail.com>
2026-04-04 01:38:48 +0100

    updated layout

---
+++ frontend/app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '@/components/Sidebar'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen bg-[#09090f] flex">
+      <Sidebar />
+      <main className="flex-1 p-8 lg:p-10 overflow-y-auto text-white">
+        {children}
+      </main>
+    </div>
+  )
+}
