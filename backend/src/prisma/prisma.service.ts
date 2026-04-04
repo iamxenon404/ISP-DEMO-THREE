6d67cc7ed776cdbffb7a5dea9cf59e54f12c67e5
xenon <demiladeafuape74@gmail.com>
2026-04-04 01:00:37 +0100

    updating prisma service to use .env

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,9 @@
-// src/prisma/prisma.service.ts
-
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 const prismaClient = new PrismaClient({ adapter })
