fe2fa67948e7199ecdc3d4756f10db66b9936f2d
xenon <demiladeafuape74@gmail.com>
2026-04-08 21:34:28 +0100

    updated app module  there is a couple errs  fimport files dont fully exist yet

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { SubscriptionsModule } from './subscriptions/subscriptions.module'
+import { PlansModule } from './plans/plans.module'
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UsersModule } from './users/users.module'
     PrismaModule,
     AuthModule,
     UsersModule,
+    SubscriptionsModule,
+    PlansModule,
   ],
 })
 export class AppModule {}
