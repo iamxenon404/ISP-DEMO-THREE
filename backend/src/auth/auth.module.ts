460214078975a26705392ab053c16319caa8a498
xenon <demiladeafuape74@gmail.com>
2026-04-15 04:35:11 +0100

    changes to fix possible  jwt  mismatch issue

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy'
   imports: [
     PassportModule,
     JwtModule.register({
-      secret:      process.env.JWT_SECRET || 'fallback-secret',
+      secret:      process.env.JWT_SECRET ,
       signOptions: { expiresIn: '7d' },
     }),
   ],
